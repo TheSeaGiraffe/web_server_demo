db66ed9756a5a1a437d9636b4e25eb275cd84406
Fahmi Adi Nugraha <fnugraha@gmail.com>
2024-08-12 08:53:34 +0700

    Create skeleton of basic webserver with no handlers.

---
+++ go.mod
@@ -0,0 +1,3 @@
+module learn_web_servers/web_server_demo
+
+go 1.22.4
---
+++ main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func main() {
+	// Setup routing
+	mux := http.NewServeMux()
+
+	// Setup and run server
+	port := "8080"
+	srv := http.Server{
+		Addr:    ":" + port,
+		Handler: mux,
+	}
+	fmt.Printf("Starting sever on port %s...\n", srv.Addr)
+	log.Fatal(srv.ListenAndServe())
+}
